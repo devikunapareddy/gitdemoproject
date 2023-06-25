82dfee6fd937dac1d659f25267455d9c63e45ebf
devi <devikunapareddy422@gmail.com>
2023-06-25 11:33:08 +0530

    first commit......

---
+++ Users/dell/eclipse-workspace/gitdemoproject/src/gitdemoproject/test.java
@@ -0,0 +1,17 @@
+package gitdemoproject;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String coding="yes";
+		if(coding=="yes")
+		{
+			System.out.println("you will get good job");
+		}
+		else
+			System.out.println("you need more practies to do coding");
+
+	}
+
+}
